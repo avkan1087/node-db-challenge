969be17af88dc2a1f410e110c4c9fbe9473e9cae
avi <avikan1087@gmail.com>
2020-01-24 13:29:52 -0500

    mvp done

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const projectsRouter = require('./projects/projects-router');
+const tasksRouter = require('./tasks/tasks-router');
+const resourcesRouter = require('./resources/resources-router');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/tasks', tasksRouter);
+server.use('/api/resources', resourcesRouter);
+
+
+module.exports = server
